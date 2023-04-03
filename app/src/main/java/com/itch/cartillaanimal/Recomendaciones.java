12d75088005dd8a35753c498e2db0ed2832e7378
2Angel000 <mscmorfy_arcangel13@hotmail.com>
2023-04-03 14:19:32 -0600

    Comit con Recomendaciones

--- app/src/main/java/com/itch/cartillaanimal/Recomendaciones.java
+++ app/src/main/java/com/itch/cartillaanimal/Recomendaciones.java
@@ -2,13 +2,31 @@ package com.itch.cartillaanimal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Recomendaciones extends AppCompatActivity {
+
+    ImageButton btnAtras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recomendaciones);
+        btnAtras = (ImageButton) findViewById(R.id.btnAtras);
+
+        btnAtras.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(Recomendaciones.this, Home.class);
+                startActivity(login);
+                finish();
+            }
+        });
     }
 }
